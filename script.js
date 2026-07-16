a8da1ab1f02fc00ab7ea862da57d3935ec474f88
Achuth N A <achuthna1@gmail.com>
2026-07-16 17:17:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -53,3 +53,33 @@ const observer = new IntersectionObserver((entries)=>{
 });
 
 hiddenElements.forEach(el=>observer.observe(el));
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
